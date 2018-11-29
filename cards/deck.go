bf7422b6bd4cd3e7a997f6becb2eb8306f64ab36
TMA\lthanhcong <lthanhcong@tma.com.vn>
2018-11-29 15:01:41 +0700

    save to file

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -35,6 +36,6 @@ func deal(d deck, handSize int) (deck, deck) {
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
-func (d deck) saveToFile(filename string) {
-
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 2)
-
-	hand.print()
-	fmt.Println("hehehehehe")
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 }
